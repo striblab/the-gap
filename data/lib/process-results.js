66a820cfb68ce2a20b440418c9ab3a67646a5a27
Alan Palazzolo <github@zzolo.org>
2017-11-14 22:02:19 -0600

    More gap analysis.

--- data/lib/process-results.js
+++ data/lib/process-results.js
@@ -4,6 +4,8 @@
  */
 
 // Dependencies
+const fs = require('fs');
+const path = require('path');
 const csv = require('fast-csv');
 const _ = require('lodash');
 
@@ -22,8 +24,8 @@ process.stdin.setEncoding('utf8');
 let proxyContest = argv['proxy-contest'] || '0101';
 let proxyContestNote = false;
 
-// Setup csv parser
-let parser = csv({
+// CSV options
+let csvOptions = {
   delimiter: ';',
   quote: '',
   headers: [
@@ -46,7 +48,17 @@ let parser = csv({
   ],
   ignoreEmpty: true,
   trim: true
-});
+};
+if (argv.type === 'legacy') {
+  csvOptions = {
+    headers: true,
+    ignoreEmpty: true,
+    trim: true
+  };
+}
+
+// Setup csv parser
+let parser = csv(csvOptions);
 
 // Collected
 let collected = {
@@ -64,52 +76,136 @@ process.stdin
     console.error(error);
     process.exit(1);
   })
-  .on('data', data => {
-    // Parse some values/numbers
-    ['precincts', 'totalPrecincts', 'votes', 'totalVotes'].forEach(p => {
-      data[p] = parseInt(data[p], 10);
-    });
-    ['percent'].forEach(p => {
-      data[p] = parseFloat(data[p]);
-    });
-
-    // Don't worry about non-R or non-D
-    if (['DFL', 'D', 'R'].indexOf(data.party) === -1) {
-      return;
-    }
-
-    // Standardize DFL
-    data.party = data.party === 'DFL' ? 'D' : data.party;
-
-    // Precinct is unique to county
-    data.precinctID = data.county + '-' + data.precinct;
-
-    // Precinct and part for easier lookup
-    data.precinctPartyID = data.county + '-' + data.precinct + '-' + data.party;
-
-    // Collect only certain sets.
-    if (data.contest === proxyContest) {
-      collected.proxy.push(data);
-
-      // Helpful for debugging
-      if (proxyContestNote !== data.contestName) {
-        console.error('Proxied contest: ', data.contestName);
-        proxyContestNote = data.contestName;
-      }
-    }
-    else if (data.contestName.match(/state senator/i)) {
-      collected.senate.push(data);
-    }
-    else if (data.contestName.match(/state representative/i)) {
-      collected.house.push(data);
-    }
-    else if (data.contestName.match(/u\.s\. representative/i)) {
-      collected.congress.push(data);
-    }
-  })
+  .on('data', argv.type === 'legacy' ? parseInputLegacy : parseInput)
   .on('end', () => {
     processCollected(collected);
   });
+
+// Parse regular
+function parseInput(data) {
+  // Parse some values/numbers
+  ['precincts', 'totalPrecincts', 'votes', 'totalVotes'].forEach(p => {
+    data[p] = parseInt(data[p], 10);
+  });
+  ['percent'].forEach(p => {
+    data[p] = parseFloat(data[p]);
+  });
+
+  // Don't worry about non-R or non-D
+  if (['DFL', 'D', 'R'].indexOf(data.party) === -1) {
+    return;
+  }
+
+  // Standardize DFL
+  data.party = data.party === 'DFL' ? 'D' : data.party;
+
+  // Precinct is unique to county
+  data.precinctID = data.county + '-' + data.precinct;
+
+  // Precinct and part for easier lookup
+  data.precinctPartyID = data.county + '-' + data.precinct + '-' + data.party;
+
+  // Collect only certain sets.
+  if (data.contest === proxyContest) {
+    collected.proxy.push(data);
+
+    // Helpful for debugging
+    if (proxyContestNote !== data.contestName) {
+      console.error('Proxied contest: ', data.contestName);
+      proxyContestNote = data.contestName;
+    }
+  }
+  else if (data.contestName.match(/state senator/i)) {
+    collected.senate.push(data);
+  }
+  else if (data.contestName.match(/state representative/i)) {
+    collected.house.push(data);
+  }
+  else if (data.contestName.match(/u\.?s\.? representative/i)) {
+    collected.congress.push(data);
+  }
+}
+
+// Parse data for legacy.  Each row is a precinct with
+// columns for each race, but we need to make a row for each
+// candidate
+function parseInputLegacy(data) {
+  let base = {
+    county: data.CountyID,
+    precinct: data[argv.year <= 1998 ? 'FIPS_VTD' : 'Precinct Code']
+  };
+  base.precinctID = base.county + '-' + base.precinct;
+
+  // Sets
+  [
+    {
+      votesPrefixField: 'CONG',
+      districtField: 'CG',
+      namePrefix: 'US Congress',
+      collection: 'congress'
+    },
+    {
+      votesPrefixField: 'MNSEN',
+      // There is no senate field, just the house, which each Senate
+      // district is divided into an A and B
+      districtField: 'LEG',
+      namePrefix: 'MN Senate',
+      collection: 'senate'
+    },
+    {
+      votesPrefixField: argv.year <= 1998 ? 'MNLeg' : 'MNLEG',
+      districtField: 'LEG',
+      namePrefix: 'MN House',
+      collection: 'house'
+    },
+    {
+      votesPrefixField: proxyContest ? proxyContest : 'USPRES',
+      districtField: '',
+      namePrefix: argv['proxy-contest-name']
+        ? argv['proxy-contest-name']
+        : 'US President',
+      collection: 'proxy'
+    }
+  ].forEach(set => {
+    [
+      { field: 'R', output: 'R' },
+      { field: 'IR', output: 'R' },
+      { field: 'D', output: 'D' },
+      { field: 'DFL', output: 'D' }
+    ].forEach(party => {
+      if (
+        !data['Precinct Name'].match(/totals/i) &&
+        data[set.votesPrefixField + party.field]
+      ) {
+        // Some hackery (see note above)
+        let district =
+          set.collection === 'senate'
+            ? data[set.districtField].substring(0, 2)
+            : data[set.districtField];
+
+        collected[set.collection].push(
+          _.extend({}, base, {
+            party: party.output,
+            votes: parseInt(data[set.votesPrefixField + party.field]),
+            totalVotes: parseInt(data[set.votesPrefixField + 'TOT']),
+            precinctPartyID: base.precinctID + '-' + party.output,
+            district: set.districtField ? data[set.districtField] : null,
+            contest: set.districtField ? district : set.votesPrefixField,
+            contestName: set.districtField
+              ? set.namePrefix + ' District ' + district
+              : set.namePrefix
+          })
+        );
+
+        // Note proxy
+        if (set.collection === 'proxy' && proxyContestNote !== set.namePrefix) {
+          console.error('Proxying: ', set.namePrefix);
+          proxyContestNote = set.namePrefix;
+        }
+      }
+    });
+  });
+}
 
 // Process the collected data
 function processCollected(data) {
@@ -122,14 +218,25 @@ function processCollected(data) {
       return;
     }
 
-    let processed = processDistricts(data[s], data.proxy);
+    // Process
+    let processed = processDistricts(s, data[s], data.proxy);
 
-    console.error(s, processed.totals);
+    // Output to file
+    fs.writeFileSync(
+      path.join(__dirname, '..', 'build', argv.year + '-' + s + '-gap.json'),
+      JSON.stringify(processed, null, '  ')
+    );
+
+    // Output
+    console.error(argv.year + ' ' + s + ': ' + processed.totals.gap);
+    console.error(processed.totals);
   });
+
+  console.error('Done.');
 }
 
 // Process districts
-function processDistricts(raw, proxy) {
+function processDistricts(type, raw, proxy) {
   // Turn proxy into better structure for lookup
   let proxyLookup = _.keyBy(proxy, 'precinctPartyID');
 
@@ -217,8 +324,11 @@ function processDistricts(raw, proxy) {
 
   // Calculate totals
   let totals = {
+    type: type,
+    year: argv.year,
     districts: districts.length,
     proxied: _.filter(districts, 'proxied').length,
+    proxyContest: proxyContestNote,
     dTotalWasted: _.sumBy(districts, 'dWasted'),
     rTotalWasted: _.sumBy(districts, 'rWasted'),
     totalVotes: _.sumBy(districts, d => {
